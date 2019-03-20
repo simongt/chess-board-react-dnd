7c51816ebcf6e18de5d8a1fac7c0770723480d8d
Sy <simon.tsegay@gmail.com>
2019-03-20 16:32:11 -0400

    updated chess set components

--- src/components/chess-set/Knight.js
+++ src/components/chess-set/Knight.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 
+/**
+ * Knight probably needs no props. It has a position, but there's no reason for the Knight to know it, because it can be positioned by being placed into a Square as a child.
+ */
+
 export class Knight extends Component {
   render() {
     return (
