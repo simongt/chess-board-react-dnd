2dc4003e3828854d4db3d2f268e5adb4a1cba2ee
Sy <simon.tsegay@gmail.com>
2019-03-20 16:18:39 -0400

    updated chess set components

---
+++ src/components/chess-set/IconSet.js
@@ -0,0 +1,64 @@
+export const iconSet = [{
+  pawn: {
+    white: {
+      icon: "♙",
+      position: { x: 0, y: 0 }
+    },
+    black: {
+      icon: "♟",
+      position: { x: 0, y: 0 }
+    }
+  },
+  rook: {
+    white: {
+      icon: "♖",
+      position: { x: 0, y: 0 }
+    },
+    black: {
+      icon: "♜",
+      position: { x: 0, y: 0 }
+    }
+  },
+  knight: {
+    white: {
+      icon: "♘",
+      position: { x: 0, y: 0 }
+    },
+    black: {
+      icon: "♞",
+      position: { x: 0, y: 0 }
+    }
+  },
+  bishop: {
+    white: {
+      icon: "♗",
+      position: { x: 0, y: 0 }
+    },
+    black: {
+      icon: "♝",
+      position: { x: 0, y: 0 }
+    }
+  },
+  queen: {
+    white: {
+      icon: "♕",
+      position: { x: 0, y: 0 }
+    },
+    black: {
+      icon: "♛",
+      position: { x: 0, y: 0 }
+    }
+  },
+  king: {
+    white: {
+      icon: "♔",
+      position: { x: 0, y: 0 }
+    },
+    black: {
+      icon: "♚",
+      position: { x: 0, y: 0 }
+    }
+  },
+}];
+
+export default { iconSet };
---
+++ src/components/chess-set/Knight.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Knight extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Knight;
