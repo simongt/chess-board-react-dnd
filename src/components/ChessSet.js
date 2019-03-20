88cc060c72ebfcb592984481ff7ecdea67334aa3
Sy <simon.tsegay@gmail.com>
2019-03-20 15:56:06 -0400

    added white and black chess pieces using emojis

---
+++ src/components/ChessSet.js
@@ -0,0 +1,28 @@
+export const chessSet = [{
+  pawn: {
+    white: "♙",
+    black: "♟",
+  },
+  rook: {
+    white: "♖",
+    black: "♜",
+  },
+  knight: {
+    white: "♘",
+    black: "♞",
+  },
+  bishop: {
+    white: "♗",
+    black: "♝",
+  },
+  queen: {
+    white: "♕",
+    black: "♛",
+  },
+  king: {
+    white: "♔",
+    black: "♚",
+  },
+}];
+
+export default { chessSet };
