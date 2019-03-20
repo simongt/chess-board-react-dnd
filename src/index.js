4356af71cd96835737e617d11175772347c43ba6
Sy <simon.tsegay@gmail.com>
2019-03-20 19:37:44 -0400

    updated entry point code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+// the React app's entry point code
 const mountNode = document.getElementById("root");
-
 ReactDOM.render(<App />, mountNode);
