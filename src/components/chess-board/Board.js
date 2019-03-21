6a5d0f30872563877571a0c19058e45677fade8f
Sy <simon.tsegay@gmail.com>
2019-03-21 06:29:04 -0400

    updated chess board components

--- src/components/chess-board/Board.js
+++ src/components/chess-board/Board.js
@@ -1,24 +1,16 @@
 import React, { Component } from "react";
 
 import Square from "./Square";
-// import Knight from "../chess-set/Knight";
 import Piece from "../chess-set/Piece";
 
 /**
- * The Board is tricky. It makes no sense to pass Squares as children to it, because what else could a board contain? 
- * 
- * Therefore it probably owns the Squares. But then, it also needs to own the Knight because this guy needs to be placed inside one of those Squares. This means that the Board needs to know the knight's current position.
- * 
- * In a real Chess game, the Board would accept a data structure describing all the pieces, their colors and positions, but for us, a knightPosition prop will suffice. We will use two-item arrays as coordinates, with [0, 0] referring to the A8 square. Why A8 instead of A1? To match the browser coordinate orientation. I tried it another way and it just messed with my head too much.
+ * Board owns the Squares, but it also needs to own Piece because each piece needs to be placed inside one of those Squares. This means that the Board needs to know the piece's current position.
  */
 
-// helper function to generate squares and pieces within it
 const renderSquare = (boardPosition, pieces) => {
   const col = boardPosition % 8;
   const row = Math.floor(boardPosition / 8);
   const blackSquare = (col + row) % 2 === 1;
-  // const pieceIsHere = (pieceCol === col) && (pieceRow === row);
-  // console.dir(pieces);
   let pieceOnSquare = null;
   const pieceIsHere = pieces.some(piece => {
     const pieceCol = piece.position[0];
@@ -26,7 +18,6 @@ const renderSquare = (boardPosition, pieces) => {
     pieceOnSquare = pieceCol === col && pieceRow === row ? piece : null;
     return (pieceCol === col && pieceRow === row);
   });
-  // const piece = pieceIsHere ? <Knight /> : null;
   const piece = pieceIsHere ? <Piece icon={pieceOnSquare.icon.fill} /> : null;
   return (
     <Square
@@ -39,16 +30,13 @@ const renderSquare = (boardPosition, pieces) => {
   );
 }
 
-// map thru initBoard and render out the squares with appropriate pieces on top
-
 class Board extends Component {
   render() {
-    const { initBoard } = this.props;
-    // console.dir(initBoard);
+    const { board } = this.props;
     const squares = [];
     const pieces = [];
-    for (const piece in initBoard) {
-      pieces.push(initBoard[piece]);
+    for (const piece in board) {
+      pieces.push(board[piece]);
     };
     
     for (let i = 0; i < 64; i++) {
--- src/components/chess-board/Square.js
+++ src/components/chess-board/Square.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 /**
- * It is tempting to give Square its position via props, but this is not necessary, because the only information it really needs for the rendering is the color. I'm going to make Square white by default, and add a black boolean prop. And of course Square may accept a single child: the chess piece that is currently on it. I chose white as the default background color to match the browser defaults.
+ * I chose white as the default background color (to match the browser defaults), so then I added a black boolean prop. Square may accept a single child, i.e. the chess piece that is currently on it.
  */
 
 class Square extends Component {
