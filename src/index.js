327dc3a894070667d7e99b558bebbb0b729ce630
Sy <simon.tsegay@gmail.com>
2019-03-21 01:55:53 -0400

    updated app's entry point

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-// import Board from "./components/chess-board/Board";
 
 // the React app's entry point code
 const mountNode = document.getElementById("root");
 ReactDOM.render(<App />, mountNode);
-// ReactDOM.render(<Board piecePosition={[0,0]} />, mountNode);
