638c24f03591fcc44950f32c2f942dabe7067324
Sy <simon.tsegay@gmail.com>
2019-03-22 02:30:05 -0400

    updated app's entry point

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./stylesheets/global.css";
 // the React app's entry point code
 const mountNode = document.getElementById("root");
 
-// wrap entire app in an observer to perform actions by the knight
+// wrap entire app in an observer to subscribe to a changing state in the most minimal, non-complex way (rather than using EventEmitter or making Game an object model), all that is needed in this case is a stream of values
 observe(knightPosition =>
  ReactDOM.render(<Board knightPosition={knightPosition} />, mountNode)
 );
