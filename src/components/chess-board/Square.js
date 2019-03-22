af0a88ccbbb50fe5977157307501d163553710ec
Sy <simon.tsegay@gmail.com>
2019-03-22 01:49:26 -0400

    updated chess board components

--- src/components/chess-board/Square.js
+++ src/components/chess-board/Square.js
@@ -1,31 +1,59 @@
-import React, { Component } from "react";
+import React from "react";
+
+export default function Square({ black, children }) {
+  const fill = black ? "black" : "white";
+  const stroke = black ? "white" : "black";
+  return (
+    <div
+      style={{
+        backgroundColor: fill,
+        color: stroke,
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
 
 /**
  * I chose white as the default background color (to match the browser defaults), so then I added a black boolean prop. Square may accept a single child, i.e. the chess piece that is currently on it.
  */
 
-class Square extends Component {
-  render() {
-    const { blackSquare, children } = this.props;
+// class Square extends Component {
+//   render() {
+//     const { blackSquare, children } = this.props;
 
-    const fill = blackSquare
-      ? "hsla(240, 66%, 10%, 1)"
-      : "hsla(25, 75%, 90%, 1)";
+//     const fill = blackSquare
+//       ? "hsla(240, 66%, 10%, 1)"
+//       : "hsla(25, 75%, 90%, 1)";
 
-    const stroke = blackSquare
-      ? "hsla(25, 75%, 90%, 1)"
-      : "hsla(240, 66%, 10%, 1)";
+//     const stroke = blackSquare
+//       ? "hsla(25, 75%, 90%, 1)"
+//       : "hsla(240, 66%, 10%, 1)";
 
-    // color shouldn't depend on square, but which player the piece belongs to, doesn't make sense to change entire color, just add an outline to the piece
-    const squareStyle = {
-      background: fill,
-      color: "transparent",
-      WebkitTextStroke: `1px ${stroke}`,
-      textAlign: "center"
-    };
+//     const squareStyle = {
+//       background: fill,
+//       color: "transparent",
+//       WebkitTextStroke: `1px ${stroke}`,
+//       textAlign: "center"
+//     };
 
-    return <div style={squareStyle}>{children}</div>;
-  }
-}
+//     const { col, row, pieces, handleSquareClick } = this.props;
+//     return (
+//       <div 
+//         style={squareStyle}
+//         onClick={() => handleSquareClick(col, row, pieces)}
+//       >
+//         {children}
+//       </div>
+//     );
+//   }
+// }
 
-export default Square;
+// export default Square;
