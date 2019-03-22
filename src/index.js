b959af41d298b3e92b4e672556aea401180051d1
Sy <simon.tsegay@gmail.com>
2019-03-22 01:49:26 -0400

    updated app's entry point

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import Board from "./components/chess-board/Board";
+import { observe } from "./components/Game";
+import "./stylesheets/global.css";
 
 // the React app's entry point code
 const mountNode = document.getElementById("root");
-ReactDOM.render(<App />, mountNode);
+
+// wrap entire app in an observer to perform actions by the knight
+observe(knightPosition =>
+ ReactDOM.render(<Board knightPosition={knightPosition} />, mountNode)
+);
